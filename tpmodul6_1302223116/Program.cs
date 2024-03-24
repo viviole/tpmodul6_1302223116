7c4cded6e6b548fabbb4576722cbbd172f3006b6
Matthew Theodore Lempas <theodorelempas@gmail.com>
2024-03-24 19:10:11 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302223116/Program.cs
+++ tpmodul6_1302223116/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul6_1302223116;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Theo");
+        for (int i = 0; i < 3; i++)
+        {
+            video.IncreasePlayCount(1);
+        }
+        video.PrintVideoDetails();
+
+    }
+}
---
+++ tpmodul6_1302223116/SayaTubeVideo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_1302223116
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(String tube)
+        {
+            Debug.Assert(tube.Length < 200);
+            Debug.Assert(!string.IsNullOrEmpty(tube));
+            Random random = new Random();
+            this.title = tube;
+            this.id = random.Next(100000);
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int playCount)
+        {
+            Debug.Assert(playCount <= 25000000);
+            try
+            {
+                checked
+                {
+                    this.playCount += playCount;
+                }
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.ToString());
+            }
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine($"ID: {id}");
+            Console.WriteLine($"Judul: {title}");
+            Console.WriteLine($"Jumlah Pemutaran: {playCount}");
+        }
+    }
+}

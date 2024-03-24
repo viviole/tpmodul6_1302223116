18014680895b91d20c7275a711c79e02205b799f
Matthew Theodore Lempas <theodorelempas@gmail.com>
2024-03-24 19:23:45 +0700

    implementasi design by contract

--- tpmodul6_1302223116/Program.cs
+++ tpmodul6_1302223116/Program.cs
@@ -5,10 +5,12 @@ internal class Program
     private static void Main(string[] args)
     {
         SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Theo");
-        for (int i = 0; i < 3; i++)
+        
+        /*Untuk uji overflow
+        for (int i = 0; i < 123413; i++)
         {
+            video.IncreasePlayCount(112390);
             video.IncreasePlayCount(1);
-        }
         video.PrintVideoDetails();
 
     }
